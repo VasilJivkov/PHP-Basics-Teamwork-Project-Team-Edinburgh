76b804b5962cb399b9dc74646848094bd41f413f
mihayloff <mihayloff14@gmail.com>
2014-12-14 21:15:45 +0200

    added the database sql

--- database/Forum.sql
+++ database/Forum.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.6deb1
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 12, 2014 at 11:20 PM
--- Server version: 5.5.40-0ubuntu1
--- PHP Version: 5.5.12-2ubuntu4.1
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2014 at 08:15 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `Forum`
+-- Database: `forum`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `Type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`Type`) VALUES
+('Programming'),
+('Women'),
+('Common Sense'),
+('Linux');
 
 -- --------------------------------------------------------
 
@@ -28,14 +48,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `posts` (
 `post_id` int(11) NOT NULL,
-  `added_by` int(11) NOT NULL,
-  `date_added` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
+  `heading` varchar(50) NOT NULL,
+  `author` varchar(50) NOT NULL,
   `content` text NOT NULL,
-  `view_count` int(11) NOT NULL,
-  `edited_by` int(11) NOT NULL,
-  `editet_when` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `category` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `heading`, `author`, `content`, `category`) VALUES
+(1, 'something', 'none', 'bla bla bla bla', 'Programming'),
+(2, 'sadsadsad', 'none', 'asdsdfsdfsdfsd', 'Programming');
 
 -- --------------------------------------------------------
 
@@ -45,25 +70,24 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 CREATE TABLE IF NOT EXISTS `users` (
 `user_id` int(11) NOT NULL,
-  `loginName` varchar(30) NOT NULL,
-  `pass` varchar(32) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `second_name` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `date_registered` int(11) NOT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT '1',
-  `is_active` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `user_name` varchar(50) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `loginName`, `pass`, `first_name`, `second_name`, `email`, `date_registered`, `type`, `is_active`) VALUES
-(1, 'namename', '827ccb0eea8a706c4c34a16891f84e7b', 'asdfsdfs', 'sdfsdfsd', 'rastamandito@mail.bg', 1418417608, 1, 1),
-(2, 'protos', '827ccb0eea8a706c4c34a16891f84e7b', 'protos', 'templar', 'sdffg@mail.bg', 1418417747, 1, 1),
-(3, 'ff', '633de4b0c14ca52ea2432a3c8a5c4c31', 'dfgdfgdfg', 'dfg', 'dfgfgdfg@vd', 1418418817, 1, 1),
-(4, 'ffdfsdfsdf', '633de4b0c14ca52ea2432a3c8a5c4c31', 'sdfsd', 'sdfdfsdf', '34234@gfde.gb', 1418418841, 1, 1);
+INSERT INTO `users` (`user_id`, `user_name`, `password`, `email`) VALUES
+(1, 'mihayloff', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'asd@asd.com'),
+(2, 'emil4o', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'emil@emil.com'),
+(3, 'qwerty', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'qwer@qwe.com'),
+(4, 'pesho', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'pesho@pesho.pesho'),
+(5, 'atanas', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'asda@adsad.com'),
+(6, 'filko', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'filko@filko.com'),
+(7, 'chico', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'chico@email.com'),
+(8, 'svetlin4o', '827ccb0eea8a706c4c34a16891f84e7b', 'svetlin@ads.com');
 
 --
 -- Indexes for dumped tables
@@ -79,7 +103,7 @@ ALTER TABLE `posts`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`user_id`), ADD KEY `second_name` (`second_name`);
+ ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -89,12 +113,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
