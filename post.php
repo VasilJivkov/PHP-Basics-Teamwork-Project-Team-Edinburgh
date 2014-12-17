1cd5605585a0ccf14054f788f61eb6e07966985b
Valentin Nikolov <nikolov.v.95@gmail.com>
2014-12-17 13:20:58 +0200

    * Comments to view post page

--- post.php
+++ post.php
@@ -23,7 +23,6 @@ $result = $db->get_results($sql);
     <script src="js/jquery-2.1.1.min.js"></script>
 
 </head>
-
 <body>
 <div id="main-container">
     <div id="left">
@@ -68,15 +67,26 @@ $result = $db->get_results($sql);
             ?>
         </div>
         <article>
-				<header><a href="javascript:;">Problem with Visual Studio, C#</a></header>
-				<em>posted by <a href="javascript:;">Administrator</a> on 14.12.2014 in <a href="javascript:;">C#</a></em>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</article>
+			<header><a href="javascript:;">Problem with Visual Studio, C#</a></header>
+			<em>posted by <a href="javascript:;">Administrator</a> on 14.12.2014 in <a href="javascript:;">C#</a></em>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</article>
+
+        <h3>Responses</h3>
+		<article class="comment">
+			<em>replied by <a href="javascript:;">Administrator</a> on 14.12.2014 13:14</em>
+			<p>Here goes the comment</p>
+		</article>
+		<article class="comment">
+			<em>replied by <a href="javascript:;">Administrator</a> on 14.12.2014 13:14</em>
+			<p>Here goes another comment</p>
+		</article>
+
         <form id="add-comment">
 			<h3>Add comment</h3>
 			<textarea rows="8"></textarea>
