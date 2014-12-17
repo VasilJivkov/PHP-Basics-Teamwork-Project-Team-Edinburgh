053fcfa6a41c908b62a2f12e15a5650f154638f3
mihayloff <mihayloff14@gmail.com>
2014-12-17 13:14:34 +0200

    added some sample content for the post page

--- post.php
+++ post.php
@@ -68,7 +68,15 @@ $result = $db->get_results($sql);
             ?>
         </div>
         <article>
-        </article>
+				<header><a href="javascript:;">Problem with Visual Studio, C#</a></header>
+				<em>posted by <a href="javascript:;">Administrator</a> on 14.12.2014 in <a href="javascript:;">C#</a></em>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</article>
         <form id="add-comment">
 			<h3>Add comment</h3>
 			<textarea rows="8"></textarea>
